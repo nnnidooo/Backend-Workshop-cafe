3b2cf60e0871b4374a51a4909a6456dfa34f1e4d
nnnidooo <nadi3252@stud.kea.dk>
2022-11-16 10:05:32 +0100

    yuuuh

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const mysql = require('mysql2');
+const cors = require('cors');
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.json());
+
+app.use(cors({
+    origin: '*'
+}));
+
+const mysqlConnection = require("./database");
+
+app.get('/database', (req, res) => {
+    mysqlConnection.query(
+        "SELECT * FROM cafes;",
+        (err, results, fields) => {
+            if (!err) {
+                res.json(results);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+})
+
+server.listen(3000,(port) => {
+    console.log('Listening on port ' + server.address().port);
+});
+
+module.exports = app;
+
